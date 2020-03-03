0c98563f335846b9c89191afe543fd50f5e4023b
943813337 <943813337@qq.com>
2020-03-03 18:42:54 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,70 @@
+Page({
+
+  data: {
+    "bnrUrl": [{
+      "url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582212006723&di=9cc0c393ccffc8b1484ab415b6dee4ad&imgtype=0&src=http%3A%2F%2Fimg.hebaomu.com%2Fdrawing%2Fupload%2Fxiaoguotu%2F20130428%2F6f45fb8c-2a28-406d-83a4-93b613606cf9.jpg"
+    }, {
+        "url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582212258696&di=4cf9b0582526135546dc24704ca64c90&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01f5f655431a540000019ae916c6de.jpg%402o.jpg"
+    }, {
+        "url": "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1091376945,1822945218&fm=26&gp=0.jpg"
+    }, {
+        "url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582212258696&di=4cf9b0582526135546dc24704ca64c90&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01f5f655431a540000019ae916c6de.jpg%402o.jpg"
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
